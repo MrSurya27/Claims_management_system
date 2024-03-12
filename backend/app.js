d1334ef1f44d11fc07958f30be3f641721541d95
MrSurya27 <110982611+MrSurya27@users.noreply.github.com>
2024-03-12 13:41:19 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'https://claimsmanagementsystemsurya.netlify.app/', 
+    origin: 'https://claimsmanagementsystemsurya.netlify.app', 
     credentials: true
 }));
 
